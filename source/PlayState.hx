1f11cc801bb8417afc6c080ad8d1655dd90ad4b9
Sam Bumgardner <Sambumgardner1@gmail.com>
2021-03-30 22:41:07 -0500

    Added basic player-controlled character.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,17 @@
 package;
 
+import actors.player.Hero;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	private var hero:Hero;
+
 	override public function create():Void
 	{
 		super.create();
+		hero = new Hero();
+		add(hero);
 	}
 
 	override public function update(elapsed:Float):Void
