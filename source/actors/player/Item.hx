c98514e816c0dd9e33f32370becc4cf076da4999
Startarrot <robertchambers4444@gmail.com>
2021-04-08 14:36:24 -0500

    Fixed Swords
    Fixed swords.  They strike in the four directions and should have proper collision boxes.

--- source/actors/player/Item.hx
+++ source/actors/player/Item.hx
@@ -3,111 +3,73 @@ package actors.player;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxObject;
+import flixel.util.FlxTimer;
 
 class Item extends FlxSprite 
 {
-    private var up:Bool = false;
-    private var down:Bool = false;
-    private var left:Bool = false;
-    private var right:Bool = false;
-    var newAngle:Float = 0;
-    var xMod:Float = 0;
-    var yMod:Float = 0;
+    private var owner:FlxSprite;
+    private var timer:FlxTimer;
 
-    public function new() {
+    private var xMod:Float = 0;
+    private var yMod:Float = 0;
+	private var time:FlxTimer;
+    
+    public function new(owner:FlxSprite) {
         super();
         makeGraphic(1,1);
+        this.owner = owner;
+        timer = new FlxTimer();
     }
     override public function update(elapsed:Float):Void
         {
-            up = FlxG.keys.pressed.UP;
-            down = FlxG.keys.pressed.DOWN;
-            left = FlxG.keys.pressed.LEFT;
-            right = FlxG.keys.pressed.RIGHT;
+            if(FlxG.keys.justPressed.A)
+                {
+                    useItem();
+                    timer.start(0.2, swing, 1);
+                }
+            pos(owner.x + 12, owner.y + 12);
 
-            if (up)
-                {
-                    newAngle = -90;
-                    if (left)
-                        newAngle -= 45;
-                    else if (right)
-                        newAngle += 45;
-                    facing = FlxObject.UP;
-                }
-                else if (down)
-                {
-                    newAngle = 90;
-                    if (left)
-                        newAngle += 45;
-                    else if (right)
-                        newAngle -= 45;
-                    facing = FlxObject.DOWN;
-                }
-                else if (left)
-                {
-                    newAngle = 180;
-                    facing = FlxObject.LEFT;
-                }
-                else if (right)
-                {
-                    newAngle = 0;
-                    facing = FlxObject.RIGHT;
-                }
+            super.update(elapsed);
         }
 
     public function useItem()
     {
-        if(newAngle == -90)
+        if(owner.facing == FlxObject.UP)
             {
-                xMod = -21;
-                yMod = -20;
+                xMod = 0;
+                yMod = -40;
+                makeGraphic(10,50);
             }
-        else if(newAngle == -135)
+        else if(owner.facing == FlxObject.DOWN)
             {
-                xMod = -35;
-                yMod = -15;
+                xMod = 0;
+                yMod = 0;
+                makeGraphic(10,50);
             }
-        else if(newAngle == -45)
-            {
-                xMod = -5;
-                yMod = -15;
-            }
-        else if(newAngle == 90)
-            {
-                xMod = -21;
-                yMod = 15;
-            }
-        else if(newAngle == 45)
-            {
-                xMod = -5;
-                yMod = 15;
-            }
-        else if(newAngle == 135)
-            {
-                xMod = -35;
-                yMod = 15;
-            }
-        else if(newAngle == 180)
+        else if(owner.facing == FlxObject.LEFT)
             {
                 xMod = -40;
                 yMod = 0;
+                makeGraphic(50,10);
             }
-
-        angle = newAngle;
-        makeGraphic(50,10);
-        
+        else if(owner.facing == FlxObject.RIGHT)
+            {
+                xMod = 0;
+                yMod = 0;
+                makeGraphic(50,10);
+            }       
     }
-
-    public function swing()
-        {
-            makeGraphic(1,1);
-            xMod = 0;
-            yMod = 0;
-        }
 
     public function pos(hX, hY)
         {
             x = hX + xMod;
             y = hY + yMod;
+        }
+
+    private function swing(timer:FlxTimer)
+        {
+            makeGraphic(1,1);
+            xMod = 0;
+            yMod = 0;
         }
 }
